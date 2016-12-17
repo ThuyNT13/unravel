cf9056093a4e09b9f87a00c088633fe401f4f105
Thuy <[01;31m[K"email"[m[K: null>
2016-12-17 14:00:46 -0600

    rename column to chapter_id from sentence_id

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,5 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+ActiveRecord::Schema.define(version: 20161217195445) do
 
-ActiveRecord::Schema.define(version: 20161217064919) do
-
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chapters", force: :cascade do |t|
@@ -25,9 +12,9 @@ ActiveRecord::Schema.define(version: 20161217064919) do
 
   create_table "rounds", force: :cascade do |t|
     t.integer  "player_id"
-    t.integer  "sentence_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sentences", force: :cascade do |t|
