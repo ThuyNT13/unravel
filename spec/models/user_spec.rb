bf769f2f4970cafce1056ceb1daed8d7ee7929bc
Thuy <[01;31m[K"email"[m[K: null>
2016-12-16 23:44:53 -0600

    add attribute specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(name: "user", email: "user@email.com", password: "password", phase: 3)}
+
+  describe "attributes" do
+    it "has a username" do
+      expect(user.username).to eq("user")
+    end
+    it "has an email" do
+      expect(user.email).to eq("user@email.com")
+    end
+    it "has a password" do
+      expect(user.password).to eq("password")
+    end
+  end
+
 end
